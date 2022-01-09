6152400a2ff23b81a5537d50c93b38112a05ef1c
LucioRR <luciorrivarola@gmail.com>
2022-01-09 01:01:27 -0300

    product detail

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
+app.get('/producto', function(req, res){
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
+
 app.listen(3000, function(){
     console.log('Servidor Levantado')
 });
