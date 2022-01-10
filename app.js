39eb0e6dfe70117282e0fc924322aa79dda49034
luliurinovsky <lic.lucilaurinovsky@gmail.com>
2022-01-10 15:22:13 -0300

    html carrito

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ let app = express();
 const path = require('path');
 app.use('/static', express.static(__dirname + '/public'));
 
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath));
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, './views/index.html'));
 });
 
 app.get('/producto', function(req, res){
     res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
+
+app.get('/carrito', function(req, res){
+    res.sendFile(path.join(__dirname, './views/productCart.html'));
 });
 
 app.get('/register', (req,res)=>{
