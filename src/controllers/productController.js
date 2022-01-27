b5f931ddb4457e841bfa55480fb87eead9138acf
LucioRR <luciorrivarola@gmail.com>
2022-01-27 01:42:44 -0300

    vista ProductEdit

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,5 +3,6 @@ const path = require('path');
 const productCart = (req, res) => res.render('productCart');
 const productDetail = (req, res) => res.render('productDetail');
 const productCreate = (req, res) => res.render('productCreate');
-
-module.exports = {productCart, productDetail, productCreate}
+const productEdit = (req, res) => res.render('productEdit');
+ 
+module.exports = {productCart, productDetail, productCreate, productEdit}
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,10 @@
 const {Router} = require('express');
 const router = Router();
-const {productCart, productDetail, productCreate} = require('../controllers/productController');
+const {productCart, productDetail, productCreate, productEdit} = require('../controllers/productController');
 
 router.get('/producto', productDetail);
 router.get('/carrito', productCart);
 router.get('/producto/crear', productCreate)
-
+router.get('/producto/editar', productEdit)
 
 module.exports = router;
