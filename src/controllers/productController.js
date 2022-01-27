6bdbb88a9741e79aff8d432f2ac06bfc365bfa56
LucioRR <luciorrivarola@gmail.com>
2022-01-27 01:24:06 -0300

    Nueva vista productCreate

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 const productCart = (req, res) => res.render('productCart');
 const productDetail = (req, res) => res.render('productDetail');
+const productCreate = (req, res) => res.render('productCreate');
 
-module.exports = {productCart, productDetail}
+module.exports = {productCart, productDetail, productCreate}
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const router = Router();
-const {productCart, productDetail} = require('../controllers/productController');
+const {productCart, productDetail, productCreate} = require('../controllers/productController');
 
 router.get('/producto', productDetail);
 router.get('/carrito', productCart);
+router.get('/producto/crear', productCreate)
 
 
 module.exports = router;
