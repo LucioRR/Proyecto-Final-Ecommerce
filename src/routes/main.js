bd6396f6652bd3a40385620cdb3437e632438a65
Saúl Belbey <88008362+sbelbey@users.noreply.github.com>
2022-01-29 22:38:02 -0300

    Terminado el index adaptable

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,10 +1,7 @@
 const {Router} = require('express');
 const router = Router();
-const {index, footer, header} = require('../controllers/mainController');
+const {index} = require('../controllers/mainController');
 
 router.get('/', index)
-router.get('/header', header)
-
-router.get('/footer', footer)
 
 module.exports = router;
