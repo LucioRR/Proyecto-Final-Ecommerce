6070437bb76f53aabf637414d108569081c7f952
EliSegoviaE <71677220+EliSegoviaE@users.noreply.github.com>
2022-01-30 22:07:59 -0300

    se agrego estilos al login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+const login = (req, res) => res.render('login');
 const register = (req, res) => res.render('register');
 
-module.exports = {register}
+module.exports = {register,login}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 const {Router} = require('express');
 const router = Router();
-const {register} = require('../controllers/userController');
+const {register, login} = require('../controllers/userController');
 
+router.get('/login', login);
 router.get('/register', register);
 
 module.exports = router;
