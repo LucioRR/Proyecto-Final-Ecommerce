bef19dbe9e259b36b6c660c2de6567777ed239a7
luliurinovsky <lic.lucilaurinovsky@gmail.com>
2022-02-03 22:11:31 -0300

    se agregan objetos literales

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,14 @@
 const {all, match} = require('../models/product')
+const productController = {
+    productCart: (req, res) => res.render('product/productCart'),
+    productDetail: (req, res) => {
+        let id = Number(req.params.id);
+        res.render('product/productDetail', {producto_id: match("id", id)});
+    },
+    productCreate: (req, res) => res.render('product/productCreate'),
+    productEdit: (req, res) => res.render('product/productEdit'),
+    productAll: (req, res) => res.render('product/allProducts', {productos: all()})
+}
 
-const productCart = (req, res) => res.render('product/productCart');
 
-const productDetail = (req, res) => {
-    let id = Number(req.params.id);
-    res.render('product/productDetail', {producto_id: match("id", id)});
-} 
-const productCreate = (req, res) => res.render('product/productCreate');
-const productEdit = (req, res) => res.render('product/productEdit');
-const productAll = (req, res) => res.render('product/allProducts', {productos: all()});
-
-
-
-module.exports = {productCart, productDetail, productCreate, productEdit, productAll}
+module.exports = productController
